0092de35d8aa0f2d9fc014793e57c6a3d7ac450c
pocem <misogrega21@gmail.com>
2024-08-05 20:26:38 +0200

    create location.html

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,9 +11,10 @@ def home_page():
     return render_template('index.html')
 
 
+@app.route("/location/<city>")
+def choose_gym(city):
 
-
-
+    return render_template('location.html', city=city)
 
 if __name__ == '__main__':
     app.run(debug=True)
